1618dd4e72debfcbd0f08512adfa21718dd9c896
ihor_hozhenko <lugtrauma@gmail.com>
2024-12-27 13:42:16 +0000

    Restored order of elements for destop

--- src/js/faq.js
+++ src/js/faq.js
@@ -20,26 +20,20 @@ document.addEventListener('DOMContentLoaded', function () {
       const icon = item.querySelector('.faq-icon');
 
       // Виправлення початкового стану іконок
-      if (content.style.display === 'none' || content.style.display === '') {
-        icon.innerHTML =
-          '<use xlink:href="./img/icons.svg#icon-arrow-bot"></use>';
-      } else {
-        icon.innerHTML =
-          '<use xlink:href="./img/icons.svg#icon-arrow-top"></use>';
-      }
-
+      const toggleButton = document.querySelector('.toggle-button');
+      const content = document.querySelector('.content');
+      const icon = document.querySelector('.icon');
       toggleButton.addEventListener('click', function () {
+        // Toggle the display of the content
         if (content.style.display === 'none' || content.style.display === '') {
           content.style.display = 'block';
-          icon.innerHTML =
-            '<use xlink:href="./img/icons.svg#icon-arrow-top"></use>';
+          icon.classList.add('rotated');
         } else {
           content.style.display = 'none';
-          icon.innerHTML =
-            '<use xlink:href="./img/icons.svg#icon-arrow-bot"></use>';
+          icon.classList.remove('rotated');
         }
       });
-      
+
       // Додаємо обробник для заголовків
       const header = item.querySelector('h3');
       header.addEventListener('click', function () {
