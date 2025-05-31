76fc57fe53d29c87b78f6245fe8966ac8151968b
Oleh Tatarynov <exocriador@gmail.com>
2025-05-31 23:44:15 +0300

    vite.config fix

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: 'src',
     build: {
       sourcemap: true,
       rollupOptions: {
@@ -34,7 +33,7 @@ export default defineConfig(({ command }) => {
           },
         },
       },
-      outDir: '../dist',
+      outDir: 'dist',
       emptyOutDir: true,
     },
     plugins: [
